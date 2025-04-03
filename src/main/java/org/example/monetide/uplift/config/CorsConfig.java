8733fd423a96b59896db2de4fcf9a37b999f5698
Whitney Heastie <wwheastie@gmail.com>
2025-04-03 00:47:01 -0500

    Add CORS

--- src/main/java/org/example/monetide/uplift/config/CorsConfig.java
+++ src/main/java/org/example/monetide/uplift/config/CorsConfig.java
@@ -12,8 +12,8 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")  // Allow all endpoints
-                        .allowedOrigins("http://localhost:5173") // Change to your frontend URL
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173", "https://monetide-frontend.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
